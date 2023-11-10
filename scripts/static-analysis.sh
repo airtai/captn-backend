224133a88a612b39e87a4332ce220049753dadfd
Kumaran Rajendhiran <kumaran@airt.ai>
2023-11-10 16:54:52 +0530

    Add pre-commit and scripts for linting, static analysis (#7)
    * Add dependency workflows
    
    * Add pre-commit and lint, static analysis scripts
    
    * Add static analysis job in workflow
    
    * Fix setup python
    
    * Remove commented lines

---
+++ scripts/static-analysis.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+# echo "Running mypy..."
+# mypy captn google_ads openai_agent application.py
+
+echo "Running bandit..."
+bandit -c pyproject.toml -r captn
+
+echo "Running semgrep..."
+semgrep scan --config auto --error
