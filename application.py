f20b4c4f6f2e0dd4e37b34ed8b07b8a06a0a7662
Kumaran Rajendhiran <kumaran@airt.ai>
2023-11-10 14:08:27 +0530

    Rename for better understanding

--- application.py
+++ application.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
 
-import google_ads_auth_service
+import google_ads
 import openai_agent
 
 app = FastAPI()
 app.include_router(openai_agent.router, tags=["OpenAI"])
-app.include_router(google_ads_auth_service.router, tags=["Google Ads"])
+app.include_router(google_ads.router, tags=["Google Ads"])
 
 if __name__ == "__main__":
     import uvicorn
