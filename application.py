4ef76e0f6e94639f844a9ffed9bdd3baeb9d9342
Davor Runje <davor@airt.ai>
2023-11-16 11:36:04 +0100

    fix

--- application.py
+++ application.py
@@ -3,14 +3,14 @@ from fastapi import FastAPI
 
 load_dotenv()
 
-import captn_agents  # noqa
+import captn  # noqa
 import google_ads  # noqa
 import openai_agent  # noqa
 
 app = FastAPI()
 app.include_router(openai_agent.router, tags=["OpenAI"])
 app.include_router(google_ads.router, tags=["Google Ads"])
-app.include_router(captn_agents.router, tags=["Captn Agents"])
+app.include_router(captn.captn_agents.router, tags=["Captn Agents"])
 
 if __name__ == "__main__":
     import uvicorn
