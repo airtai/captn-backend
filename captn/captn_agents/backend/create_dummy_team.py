083f8db678dc55e0bba08d66d3293e5314614819
Harish Mohan Raj <harish@airt.ai>
2023-11-24 15:59:59 +0530

    Update mock logic

--- captn/captn_agents/backend/create_dummy_team.py
+++ captn/captn_agents/backend/create_dummy_team.py
@@ -3,25 +3,39 @@ import asyncio
 # Global variable to keep track of the status of tasks
 TASK_STATUS = {}
 
+QUESTION_MSG = """
+## 📢 Notification from our team: 
+
+👋 Hey there!
+
+Our team has a question for you. Can you please answer the below:
+
+What is your name? 😊"""
+
+ANSWER_MSG = """
+## 📢 Notification from our team: 
+
+👋 Hey there!
+
+Your campaign report is ready😊"""
+
 async def execute_dummy_task(conversation_id):
     global TASK_STATUS
 
-    if conversation_id not in TASK_STATUS:
+    if (conversation_id not in TASK_STATUS) or (not TASK_STATUS[conversation_id]["is_question"]):
         # If conversation_id is not in TASK_STATUS, create new entry
-        TASK_STATUS[conversation_id] = {"status": "inprogress", "msg": ""}
+        TASK_STATUS[conversation_id] = {"status": "inprogress", "msg": "", "is_question": True}
         await asyncio.sleep(15)
-        TASK_STATUS[conversation_id] = {"status": "ready", "msg": "I have a question. What is your name?"}
+        TASK_STATUS[conversation_id]["status"] = "ready"
+        TASK_STATUS[conversation_id]["msg"] = QUESTION_MSG
     else:
         # If conversation_id already exists in TASK_STATUS, reset and update
-        TASK_STATUS[conversation_id] = {"status": "inprogress", "msg": ""}
+        TASK_STATUS[conversation_id] = {"status": "inprogress", "msg": "", "is_question": not TASK_STATUS[conversation_id]["is_question"]}
         await asyncio.sleep(20)
-        TASK_STATUS[conversation_id] = {"status": "ready", "msg": "Nevermind. I know the answer already."}
+        TASK_STATUS[conversation_id]["status"] = "ready"
+        TASK_STATUS[conversation_id]["msg"] = ANSWER_MSG
 
 def create_dummy_task(conversation_id, message):
-    print("======")
-    print(f"New task is created: {conversation_id}")
-    print(f"Message: {message}")
-    print("======")
     # Start the task execution with the given conversation_id
     asyncio.create_task(execute_dummy_task(conversation_id))
 
--- tests/captn_agents/test_create_dummy_team.py
+++ tests/captn_agents/test_create_dummy_team.py
@@ -4,6 +4,23 @@ import pytest
 import asyncio
 from captn.captn_agents.backend.create_dummy_team import create_dummy_task, get_dummy_task_status
 
+QUESTION_MSG = """
+## 📢 Notification from our team: 
+
+👋 Hey there!
+
+Our team has a question for you. Can you please answer the below:
+
+What is your name? 😊"""
+
+ANSWER_MSG = """
+## 📢 Notification from our team: 
+
+👋 Hey there!
+
+Your campaign report is ready😊"""
+
+
 @pytest.mark.asyncio
 async def test_dummy_task_creation():
     conversation_id = "test_conv_1"
@@ -12,17 +29,24 @@ async def test_dummy_task_creation():
     
     # Initially, the status should be inprogress
     await asyncio.sleep(1)
-    assert get_dummy_task_status(conversation_id) == {"status": "inprogress", "msg": ""}
+    assert get_dummy_task_status(conversation_id) == {"status": "inprogress", "msg": "", "is_question": True}
     
     await asyncio.sleep(16)  # Wait for the first task to complete (15 seconds + 1 second buffer)
-    assert get_dummy_task_status(conversation_id) == {"status": "ready", "msg": "I have a question. What is your name?"}
+    assert get_dummy_task_status(conversation_id) == {"status": "ready", "msg": QUESTION_MSG, "is_question": True}
 
     # call create_dummy_task for the second time
     create_dummy_task(conversation_id, message)
     
     # After the second creation, the status should reset to inprogress
     await asyncio.sleep(1)
-    assert get_dummy_task_status(conversation_id) == {"status": "inprogress", "msg": ""}
+    assert get_dummy_task_status(conversation_id) == {"status": "inprogress", "msg": "", "is_question": False}
     
     await asyncio.sleep(21)  # Wait for the second task to complete (20 seconds + 1 second buffer)
-    assert get_dummy_task_status(conversation_id) == {"status": "ready", "msg": "Nevermind. I know the answer already."}
+    assert get_dummy_task_status(conversation_id) == {"status": "ready", "msg": ANSWER_MSG, "is_question": False}
+
+
+    # call create_dummy_task for the second time
+    create_dummy_task(conversation_id, message)
+
+    await asyncio.sleep(16)  # Wait for the first task to complete (15 seconds + 1 second buffer)
+    assert get_dummy_task_status(conversation_id) == {"status": "ready", "msg": QUESTION_MSG, "is_question": True}
