a619ffa49f583abb796af70d76ff0a29a42685db
Harish Mohan Raj <harish@airt.ai>
2023-12-13 11:15:53 +0530

    WIP

--- captn/captn_agents/backend/create_dummy_team.py
+++ captn/captn_agents/backend/create_dummy_team.py
@@ -18,23 +18,30 @@ NOTIFICATION_MSG = """
 TASK_STATUS: Dict[str, Dict[str, Union[str, bool]]] = {}
 
 
-def create_new_task(conversation_id: int, user_id: int, chat_id: int, team_name: str) -> None:
+def create_new_task(
+    conversation_id: int, user_id: int, chat_id: int, team_name: str
+) -> None:
     TASK_STATUS[conversation_id] = {
-        "team_id":conversation_id,
-        "user_id":user_id,
-        "chat_id":chat_id,
-        "team_name":team_name,
-        "team_status":"inprogress",
-        "msg":"",
-        "is_question":False,
+        "team_id": conversation_id,
+        "user_id": user_id,
+        "chat_id": chat_id,
+        "team_name": team_name,
+        "team_status": "inprogress",
+        "msg": "",
+        "is_question": False,
     }
 
-def update_task(conversation_id: int, team_status: str, message: str, is_question: bool) -> None:
-    TASK_STATUS[conversation_id].update({
-        "team_status": team_status,
-        "msg": message,
-        "is_question": is_question,
-    })
+
+def update_task(
+    conversation_id: int, team_status: str, message: str, is_question: bool
+) -> None:
+    TASK_STATUS[conversation_id].update(
+        {
+            "team_status": team_status,
+            "msg": message,
+            "is_question": is_question,
+        }
+    )
 
 
 def chat_with_team(message: str, user_id: int, conversation_id: int) -> Dict[str, any]:
@@ -44,17 +51,17 @@ def chat_with_team(message: str, user_id: int, conversation_id: int) -> Dict[str
     return ast.literal_eval(chat(request_obj))
 
 
-
-def execute_dummy_task(user_id: int, chat_id: int, conversation_id: int, team_name: str, message: str) -> None:
-    
+def execute_dummy_task(
+    user_id: int, chat_id: int, conversation_id: int, team_name: str, message: str
+) -> None:
     is_new_task = conversation_id not in TASK_STATUS
 
     if is_new_task:
         create_new_task(conversation_id, user_id, chat_id, team_name)
     else:
-        team_status="inprogress"
-        message=""
-        is_question=False
+        team_status = "inprogress"
+        message = ""
+        is_question = False
         update_task(conversation_id, team_status, message, is_question)
 
     response = chat_with_team(message, user_id, conversation_id)
