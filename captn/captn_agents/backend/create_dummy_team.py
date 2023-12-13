9a8c399c7a8f10fd8dc0813dfb29dede1009830b
Harish Mohan Raj <harish@airt.ai>
2023-12-13 16:57:01 +0530

    Use Global variable to store team status

--- captn/captn_agents/backend/create_dummy_team.py
+++ captn/captn_agents/backend/create_dummy_team.py
@@ -15,13 +15,13 @@ NOTIFICATION_MSG = """
 <br/>
 """
 
-TASK_STATUS: Dict[str, Dict[str, Union[str, bool]]] = {}
+TASK_STATUS: Dict[str, Dict[str, Union[str, bool, int]]] = {}
 
 
 def create_new_task(
     conversation_id: int, user_id: int, chat_id: int, team_name: str
 ) -> None:
-    TASK_STATUS[conversation_id] = {
+    TASK_STATUS[f"{conversation_id}"] = {
         "team_id": conversation_id,
         "user_id": user_id,
         "chat_id": chat_id,
@@ -35,7 +35,7 @@ def create_new_task(
 def update_task(
     conversation_id: int, team_status: str, message: str, is_question: bool
 ) -> None:
-    TASK_STATUS[conversation_id].update(
+    TASK_STATUS[f"{conversation_id}"].update(
         {
             "team_status": team_status,
             "msg": message,
@@ -44,32 +44,35 @@ def update_task(
     )
 
 
-def chat_with_team(message: str, user_id: int, conversation_id: int) -> Dict[str, any]:
+def chat_with_team(
+    message: str, user_id: int, conversation_id: int
+) -> Dict[str, Union[str, bool]]:
     request_obj = CaptnAgentRequest(
         message=message, user_id=user_id, conv_id=conversation_id
     )
-    return ast.literal_eval(chat(request_obj))
+    response: Dict[str, Union[str, bool]] = ast.literal_eval(chat(request_obj))
+    return response
 
 
 def execute_dummy_task(
     user_id: int, chat_id: int, conversation_id: int, team_name: str, message: str
 ) -> None:
-    is_new_task = conversation_id not in TASK_STATUS
+    is_new_task = str(conversation_id) not in TASK_STATUS
 
     if is_new_task:
         create_new_task(conversation_id, user_id, chat_id, team_name)
     else:
         team_status = "inprogress"
-        message = ""
+        message = message
         is_question = False
         update_task(conversation_id, team_status, message, is_question)
 
     response = chat_with_team(message, user_id, conversation_id)
     update_task(
         conversation_id=conversation_id,
-        team_status=response["status"],
+        team_status=response["status"],  # type: ignore
         message=NOTIFICATION_MSG.format(team_name, response["message"]),
-        is_question=response["is_question"],
+        is_question=response["is_question"],  # type: ignore
     )
 
 
@@ -96,4 +99,4 @@ async def create_dummy_task(
 async def get_dummy_task_status(
     conversation_id: int,
 ) -> Dict[str, Union[str, bool, int]]:
-    return TASK_STATUS.get(conversation_id, {})
+    return TASK_STATUS.get(str(conversation_id), {})
