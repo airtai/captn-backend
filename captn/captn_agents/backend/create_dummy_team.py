1ea96f93cd8ae58a8ac978d95c37106a600657c4
Harish Mohan Raj <harish@airt.ai>
2023-12-13 11:15:12 +0530

    WIP

--- captn/captn_agents/backend/create_dummy_team.py
+++ captn/captn_agents/backend/create_dummy_team.py
@@ -1,13 +1,9 @@
 import ast
 from typing import Dict, Union
 
-from asyncer import asyncify
 from fastapi import BackgroundTasks
-from prisma.errors import RecordNotFoundError
-from prisma.models import Task
 
 from captn.captn_agents.application import CaptnAgentRequest, chat
-from captn.captn_agents.helpers import get_db_connection
 
 NOTIFICATION_MSG = """
 ## 📢 Notification from **{}**:
@@ -19,91 +15,55 @@ NOTIFICATION_MSG = """
 <br/>
 """
 
-
-async def create_task(
-    *,
-    team_id: int,
-    user_id: int,
-    chat_id: int,
-    team_name: str,
-    team_status: str,
-    msg: str,
-    is_question: bool,
-) -> Task:
-    data = locals()
-    async with get_db_connection() as db:  # type: ignore[var-annotated]
-        task: Task = await db.task.create(data=data)
-    return task
+TASK_STATUS: Dict[str, Dict[str, Union[str, bool]]] = {}
 
 
-async def get_task(*, team_id: int) -> Task:
-    async with get_db_connection() as db:  # type: ignore[var-annotated]
-        task: Task = await db.task.find_unique_or_raise(where={"team_id": team_id})
-    return task
+def create_new_task(conversation_id: int, user_id: int, chat_id: int, team_name: str) -> None:
+    TASK_STATUS[conversation_id] = {
+        "team_id":conversation_id,
+        "user_id":user_id,
+        "chat_id":chat_id,
+        "team_name":team_name,
+        "team_status":"inprogress",
+        "msg":"",
+        "is_question":False,
+    }
+
+def update_task(conversation_id: int, team_status: str, message: str, is_question: bool) -> None:
+    TASK_STATUS[conversation_id].update({
+        "team_status": team_status,
+        "msg": message,
+        "is_question": is_question,
+    })
 
 
-async def update_task(team_id: int, **kwargs: Union[int, str, bool]) -> Task:
-    async with get_db_connection() as db:  # type: ignore[var-annotated]
-        task: Task = await db.task.update(where={"team_id": team_id}, data=kwargs)
-    return task
-
-
-async def create_new_task(
-    user_id: int, chat_id: int, conversation_id: int, team_name: str, message: str
-) -> Task:
-    task = await create_task(
-        team_id=conversation_id,
-        user_id=user_id,
-        chat_id=chat_id,
-        team_name=team_name,
-        team_status="inprogress",
-        msg="",
-        is_question=False,
-    )
-    return task
-
-
-async def update_existing_task(
-    task: Task, user_id: int, conversation_id: int, message: str, team_name: str
-) -> Task:
-    task = await update_task(
-        team_id=conversation_id,
-        team_name=team_name,
-        team_status="inprogress",
-        msg="",
-        is_question=False,
-    )
+def chat_with_team(message: str, user_id: int, conversation_id: int) -> Dict[str, any]:
     request_obj = CaptnAgentRequest(
         message=message, user_id=user_id, conv_id=conversation_id
     )
+    return ast.literal_eval(chat(request_obj))
 
-    # response = ast.literal_eval(chat(request_obj))
 
-    response = await asyncify(chat)(request_obj)
-    response_dict = ast.literal_eval(response)
 
-    last_message = response_dict["message"]
-    status = response_dict["status"]
-    is_question = response_dict["is_question"]
-    task = await update_task(
-        team_id=task.team_id,
-        team_status=status,
-        msg=NOTIFICATION_MSG.format(team_name, last_message),
-        is_question=is_question,
+def execute_dummy_task(user_id: int, chat_id: int, conversation_id: int, team_name: str, message: str) -> None:
+    
+    is_new_task = conversation_id not in TASK_STATUS
+
+    if is_new_task:
+        create_new_task(conversation_id, user_id, chat_id, team_name)
+    else:
+        team_status="inprogress"
+        message=""
+        is_question=False
+        update_task(conversation_id, team_status, message, is_question)
+
+    response = chat_with_team(message, user_id, conversation_id)
+    update_task(
+        conversation_id=conversation_id,
+        team_status=response["status"],
+        message=NOTIFICATION_MSG.format(team_name, response["message"]),
+        is_question=response["is_question"],
     )
-    return task
-
-
-async def execute_dummy_task(
-    user_id: int, chat_id: int, conversation_id: int, team_name: str, message: str
-) -> None:
-    try:
-        task = await get_task(team_id=conversation_id)
-    except RecordNotFoundError:
-        task = await create_new_task(
-            user_id, chat_id, conversation_id, team_name, message
-        )
-    await update_existing_task(task, user_id, conversation_id, message, team_name)
 
 
 async def create_dummy_task(
@@ -117,28 +77,16 @@ async def create_dummy_task(
     print("======")
     print("New team is created with the following details:")
     print(f"User ID: {user_id}")
-    print(f"Chat ID: {chat_id}")
     print(f"Team ID/Conversation ID: {conversation_id}")
     print(f"Team Name: {team_name}")
     print(f"Message: {message}")
     print("======")
-
     background_tasks.add_task(
         execute_dummy_task, user_id, chat_id, conversation_id, team_name, message
     )
-    # asyncio.create_task(
-    #     execute_dummy_task(user_id, chat_id, conversation_id, team_name, message)
-    # )
 
 
 async def get_dummy_task_status(
     conversation_id: int,
 ) -> Dict[str, Union[str, bool, int]]:
-    try:
-        task = await get_task(team_id=conversation_id)
-        d = task.model_dump()
-        exclude_columns = ["created_at", "updated_at"]
-        result = {k: v for k, v in d.items() if k not in exclude_columns}  # type: ignore
-        return result
-    except RecordNotFoundError:
-        return {}
+    return TASK_STATUS.get(conversation_id, {})
