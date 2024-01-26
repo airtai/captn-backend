4b78b1222212ee0df97685b3cbb13875d21b8f7a
rjambrecic <32619626+rjambrecic@users.noreply.github.com>
2024-01-26 15:38:09 +0100

    Update user proxy within the get_info_from_the_web_page to use the gpt-3.5 model and promp update (#252)

--- captn/captn_agents/backend/functions.py
+++ captn/captn_agents/backend/functions.py
@@ -55,13 +55,13 @@ def reply_to_client_2(
     return return_msg
 
 
-llm_config = {
+llm_config_gpt_4 = {
     "timeout": 600,
     "config_list": config_list_gpt_4,
     "temperature": 0,
 }
 
-summarizer_llm_config = {
+llm_config_gpt_3_5 = {
     "timeout": 600,
     "config_list": config_list_gpt_3_5,
     "temperature": 0,
@@ -78,31 +78,45 @@ def get_info_from_the_web_page(url: str, task: str, task_guidelines: str) -> str
 
     web_surfer = WebSurferAgent(
         "web_surfer",
-        llm_config=llm_config,
-        summarizer_llm_config=summarizer_llm_config,
+        llm_config=llm_config_gpt_4,
+        summarizer_llm_config=llm_config_gpt_3_5,
         browser_config={"viewport_size": 4096, "bing_api_key": None},
         is_termination_msg=lambda x: "SUMMARY" in x["content"]
         or "FAILED" in x["content"],
     )
 
     user_system_message = f"""You are in charge of navigating the web_surfer agent to scrape the web.
-web_surfer is able to click on links, scroll down, and scrape the content of the web page. e.g. you cen tell him: "Click the 'Getting Started' result".
-Do NOT try to click all the links on the page, but only the ones which are RELEVANT for the task! Web pages can be very long and you will be penalized if spend too much time on this task!
-Your final goal is to summarize the findings for the given task. The summary must be in English!
-GUIDELINES: {task_guidelines}
+web_surfer is able to CLICK on links, SCROLL down, and scrape the content of the web page. e.g. you cen tell him: "Click the 'Getting Started' result".
+Each time you receive a reply from web_surfer, you need to tell him what to do next. e.g. "Click the TV link" or "Scroll down".
+It is very important that you explore ALL the page links relevant for the task!
 
-It is also useful to include in the summary relevant links where more information can be found.
+GUIDELINES:
+- {task_guidelines}
+- Once you retrieve the content from the received url, you can tell web_surfer to CLICK on links, SCROLL down...
+By using these capabilities, you will be able to retrieve MUCH BETTER information from the web page than by just scraping the given URL!
+You MUST use these capabilities when you receive a task for a specific category/product etc.
+Examples:
+"Click the 'TVs' result" - This way you will navigate to the TVs section of the page and you will find more information about TVs.
+"Click 'Electronics' link" - This way you will navigate to the Electronics section of the page and you will find more information about Electronics.
+"Cick the 'Next' button"
+
+- Do NOT try to click all the links on the page, but only the ones which are RELEVANT for the task! Web pages can be very long and you will be penalized if spend too much time on this task!
+- Your final goal is to summarize the findings for the given task. The summary must be in English!
+- Create a summary after you have retrieved ALL the relevant information from ALL the pages which you think are relevant for the task!
+- It is also useful to include in the summary relevant links where more information can be found.
 e.g. If the page is offering to sell TVs, you can include a link to the TV section of the page.
+- If you get some 40x error, please do NOT give up immediately, but try to navigate to another page and continue with the task. Give up only if you get 40x error on ALL the pages which you tried to navigate to.
 
+
+FINAL MESSAGE:
 If successful, your last message needs to start with "SUMMARY:" and then you need to write the summary.
 Note: NEVER suggest the next steps in the summary! It is NOT your job to do that!
 
-
 Suggest 15 relevant headlines where each headline has MAXIMUM 30 characters (including spaces). (headlines are often similar to keywords)
 Suggest 4 relevant descriptions where each description has MAXIMUM 90 characters (including spaces).
-Pay attention to the length of the headlines and descriptions! You will be penalized if you exceed the limits!
 
-Example:
+Example
+'''
 SUMMARY:
 
 Page content: The store offers a variety of electronic categories including Air Conditioners, Kitchen Appliances, PCs & Laptops, Gadgets, Smart Home devices, Home Appliances, Audio & Video equipment, and Refrigerators.
@@ -118,11 +132,13 @@ Relevant links:
 - New Arrivals: https://store-eg.net/new-arrivals/
 
 Keywords: store, electronic, Air Conditioners, Kitchen Appliances, PCs & Laptops, Gadgets, Smart Home devices, Home Appliances, Audio & Video equipment, Refrigerators
-Headlines: store, electronic, Air Conditioners, Kitchen Appliances, PCs & Laptops, Gadgets, Smart Home devices, Home Appliances, Audio & Video equipment, Refrigerators, New Arrivals
-Descriptions: Best store for electronic devices in Europe, Buy electronic devices online, Save 20% on electronic devices, Top quality electronic devices
+Headlines (MAX 30 char each): store, electronic, Air Conditioners, Kitchen Appliances, PCs & Laptops, Gadgets, Smart Home devices, Home Appliances, Audio & Video equipment, Refrigerators, New Arrivals
+Descriptions (MAX 90 char each): Best store for electronic devices in Europe, Buy electronic devices online, Save 20% on electronic devices, Top quality electronic devices
+(VERY IMPORTANT: each headline has LESS than 30 characters and each description has LESS than 90 characters)
 
+Use these information to SUGGEST the next steps to the client, but do NOT make any permanent changes without the client's approval!
+'''
 
-If you get some 40x error, please do NOT give up immediately, but try to navigate to another page and continue with the task. Give up only if you get 40x error on all the pages which you tried to navigate to.
 Otherwise, your last message needs to start with "FAILED:" and then you need to write the reason why you failed.
 You shold respond with 'FAILED' ONLY if you were NOT able to retrieve ANY information from the web page! Otherwise, you should respond with 'SUMMARY' and the summary of your findings.
 """
@@ -130,7 +146,7 @@ You shold respond with 'FAILED' ONLY if you were NOT able to retrieve ANY inform
         "user_proxy",
         human_input_mode="NEVER",
         code_execution_config=False,
-        llm_config=llm_config,
+        llm_config=llm_config_gpt_3_5,
         system_message=user_system_message,
     )
 
