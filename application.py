e4e97f0497d1fabf2e2e275e997b085fc9ee11b5
rjambrecic <32619626+rjambrecic@users.noreply.github.com>
2024-02-01 13:52:14 +0100

    Cronjob time changed (#277)

--- application.py
+++ application.py
@@ -18,7 +18,7 @@ import openai_agent  # noqa
 async def lifespan(app: FastAPI) -> AsyncGenerator:  # type: ignore
     scheduler = BackgroundScheduler()
     scheduler.add_job(
-        execute_daily_analysis, "cron", hour="*/12", minute="*/45"
+        execute_daily_analysis, "cron", minute="*/5"
     )  # second="*/59") # use hour="*/22" for production
     scheduler.start()
     yield
