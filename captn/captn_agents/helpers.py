b4feb3cc8a34c832e087237ccad8edbc1ffc96ec
Robert Jambrecic <robert@airt.ai>
2024-02-12 15:16:16 +0100

    Add connect_timeout parameter to database url

--- captn/captn_agents/helpers.py
+++ captn/captn_agents/helpers.py
@@ -9,6 +9,8 @@ from prisma import Prisma  # type: ignore[attr-defined]
 async def get_db_connection(db_url: Optional[str] = None) -> Prisma:  # type: ignore
     if not db_url:
         db_url = environ.get("DATABASE_URL")
+    if "connect_timeout" not in db_url:  # type: ignore
+        db_url += "?connect_timeout=60"  # type: ignore
     db = Prisma(datasource={"url": db_url})  # type: ignore
     await db.connect()
     try:
@@ -21,4 +23,6 @@ async def get_wasp_db_url() -> str:
     curr_db_url = environ.get("DATABASE_URL")
     wasp_db_name = environ.get("WASP_DB_NAME", "waspdb")
     wasp_db_url = curr_db_url.replace(curr_db_url.split("/")[-1], wasp_db_name)  # type: ignore[union-attr]
+    if "connect_timeout" not in wasp_db_url:  # type: ignore
+        wasp_db_url += "?connect_timeout=60"
     return wasp_db_url
