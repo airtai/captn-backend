d781e45e0297c1174364f74af1ff3b0224e9d868
rjambrecic <32619626+rjambrecic@users.noreply.github.com>
2024-03-06 15:12:15 +0100

    Add iostream to the webpage scraping team (#424)
    * Add iostream to the webpage scraping team
    
    * Prompt update

--- captn/captn_agents/backend/google_ads_team.py
+++ captn/captn_agents/backend/google_ads_team.py
@@ -145,6 +145,7 @@ sure it is understandable by non-experts.
             conv_id=conv_id,
             work_dir=work_dir,
             clients_question_answere_list=clients_question_answere_list,
+            iostream=iostream,
         )
         roles: List[Dict[str, str]] = GoogleAdsTeam._default_roles
 
@@ -288,7 +289,7 @@ You can use the following query for retrieving the local currency: SELECT custom
 41. If the clients message contains '### Proposed User Action ###' and '### User Action ###', proceed immediately with the task in the '### User Action ###'
 You can find most od the information in the '### History ###' section and there is probably no need to execute the 'execute_query' command or 'get_info_from_the_web_page' command.
 After finishing the task in the '### User Action ###' section, you can go back to the '### Proposed User Action ###' section and make suggestions for the next steps.
-42. Finally, ensure that your responses are formatted using markdown syntax (except for the '<a href= ...</a>' links),
+42. Finally, ensure that your responses are formatted using markdown syntax (except for the HTML anchor tags),
 as they will be featured on a webpage to ensure a user-friendly presentation.
 
 Here is a list of things which you CAN do:
@@ -535,6 +536,7 @@ def _get_function_map(
     conv_id: int,
     work_dir: str,
     clients_question_answere_list: List[Tuple[str, Optional[str]]],
+    iostream: Optional[IOWebsockets] = None,
 ) -> Dict[str, Any]:
     def _string_to_list(
         customer_ids: Optional[Union[List[str], str]]
@@ -806,7 +808,9 @@ def _get_function_map(
             ),
             endpoint="/create-update-ad-copy",
         ),
-        "get_info_from_the_web_page": get_info_from_the_web_page,
+        "get_info_from_the_web_page": lambda url, task, task_guidelines: get_info_from_the_web_page(
+            url=url, task=task, task_guidelines=task_guidelines, iostream=iostream
+        ),
     }
 
     return function_map
