e516d682ca2b43bc7b5a73df670f46ee13984d17
Kumaran Rajendhiran <kumaran@airt.ai>
2024-03-25 16:21:31 +0530

    Add as script to run grafana with docker compose

---
+++ scripts/run_docker_compose.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+docker build --progress=plain --build-arg PORT=9000 -t ghcr.io/airtai/captn-backend:dev .
+
+mkdir -p grafana_data loki_data prometheus_data
+
+docker-compose up -d
+
+echo "All docker containers started. Grafana is running on http://localhost:3000/d/fastapi-observability/fastapi-observability?orgId=1&refresh=5s"
+
+echo "To stop the containers, run 'docker-compose down'"
