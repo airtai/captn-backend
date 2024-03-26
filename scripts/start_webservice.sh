ce8305540cbc288f1035aec7ab345afeecfd3a7c
Kumaran Rajendhiran <kumaran@airt.ai>
2024-03-26 16:40:44 +0530

    Run websocket server and fastapi together to prevent loss of counter metrics

--- scripts/start_webservice.sh
+++ scripts/start_webservice.sh
@@ -12,7 +12,7 @@ cat <<< "$CLIENT_SECRET" > client_secret.json
 prisma migrate deploy
 prisma generate
 
-python3 ws_application.py > ws.log 2>&1 &
-tail -f ws.log &
+# python3 ws_application.py > ws.log 2>&1 &
+# tail -f ws.log &
 
 uvicorn application:app --port $PORT --host 0.0.0.0 --workers=$NUM_WORKERS --proxy-headers --lifespan on --log-config uvicorn-log-config.json
