a74eec68602c4a3b77906310e62856510047e476
Kumaran Rajendhiran <kumaran@airt.ai>
2024-03-27 14:35:20 +0530

    Add separate counter for ping requests (#566)
    * Add separate counter for ping requests
    
    * Count non-ping requests with WEBSOCKET_REQUESTS counter
    
    * Fix typo

--- captn/observability/websocket_utils.py
+++ captn/observability/websocket_utils.py
@@ -5,3 +5,5 @@ WEBSOCKET_REQUESTS = Counter(
 )
 
 WEBSOCKET_TOKENS = Counter("websocket_tokens_total", "Total count of websocket tokens")
+
+PING_REQUESTS = Counter("ping_requests_total", "Total count of ping requests")
