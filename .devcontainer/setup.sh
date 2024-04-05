b88d811f910e633a8ba7cb3114c6c74044f9e9db
Davor Runje <davor@airt.ai>
2024-04-05 17:39:53 +0200

    Update devcontainer (#611)
    * add devcontainer
    
    * Changes to be committed:
        new file:   .devcontainer/devcontainer.json
        modified:   .gitignore
    
    * post fix
    
    * fixing
    
    ---------
    
    Co-authored-by: Kumaran Rajendhiran <kumaran@airt.ai>

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -17,3 +17,18 @@ pre-commit install
 
 # install prisma
 prisma generate
+
+echo '{
+    "web": {
+        "client_id": "571449075812-2ehtrjb3k2vsqh6fl10mu3d3oruau1t2.apps.googleusercontent.com",
+        "project_id": "captn-chat-prod",
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_secret": "'${GOOGLE_ADS_CLIENT_SECRET}'",
+        "redirect_uris": [
+            "http://localhost:9000/login/callback"
+        ]
+
+    }
+}' > client_secret.json
