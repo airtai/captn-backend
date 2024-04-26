33fab8d3ef580e5e29f5313f36ed42b3a2e77f54
Robert Jambrecic <robert@airt.ai>
2024-04-26 15:15:36 +0200

    wip

--- tests/benchmark/get_info_from_the_web_page.py
+++ tests/benchmark/get_info_from_the_web_page.py
@@ -1,11 +1,11 @@
 import os
 import sys
 import time
+from contextlib import contextmanager
 from enum import Enum
 from pathlib import Path
-from typing import Any, Dict
+from typing import Any, Dict, Iterator
 
-import numpy as np
 import pandas as pd
 import typer
 from filelock import FileLock
@@ -61,6 +61,7 @@ def run_test(
             # "outer_retries": outer_retries,
             "inner_retries": inner_retries,
             "last_message": last_message,
+            "status": "DONE",
         }
 
     return result
@@ -78,6 +79,7 @@ def _get_file_name(file_suffix: str) -> str:
 
 
 def create_ag_report(df: pd.DataFrame) -> pd.DataFrame:
+    df = df.dropna(subset=["success"])
     group = df.groupby("url")["success"]
     total = group.count()
     success = group.sum()
@@ -91,6 +93,17 @@ def create_ag_report(df: pd.DataFrame) -> pd.DataFrame:
     )
 
 
+@contextmanager
+def lock_file(file_suffix: str) -> Iterator[Path]:
+    file_name_all_tests = _get_file_name(file_suffix)
+    report_all_runs_path = REPORTS_FOLDER / file_name_all_tests
+    report_all_runs_path_lock = REPORTS_FOLDER / f"{file_name_all_tests}.lock"
+
+    with FileLock(str(report_all_runs_path_lock)):
+        yield report_all_runs_path
+    report_all_runs_path_lock.unlink()
+
+
 @app.command()
 def generate_aggregated_report(
     file_suffix: str = typer.Option(
@@ -100,42 +113,68 @@ def generate_aggregated_report(
         help="File suffix for the aggregated report",
     ),
 ):
-    file_name_all_tests = _get_file_name(file_suffix)
-    report_all_runs_path = REPORTS_FOLDER / file_name_all_tests
-    df = pd.read_csv(report_all_runs_path)
-    report_df = create_ag_report(df)
+    with lock_file(file_suffix) as report_all_runs_path:
+        df = pd.read_csv(report_all_runs_path)
+        report_df = create_ag_report(df)
 
-    report_aggregated_path = (
-        REPORTS_FOLDER / f"get_info_from_the_web_page_aggregated_{file_suffix}.csv"
-    )
-    report_df.to_csv(report_aggregated_path)
+        report_aggregated_path = (
+            REPORTS_FOLDER / f"get_info_from_the_web_page_aggregated_{file_suffix}.csv"
+        )
+        report_df.to_csv(report_aggregated_path)
 
 
-def generate_reports(results: Dict[str, Any], file_suffix: str):
+def generate_reports(result: Dict[str, Any], file_suffix: str):
+    with lock_file(file_suffix) as report_all_runs_path:
+        df = pd.read_csv(report_all_runs_path)
+        # Delete the row with the same id
+        df = df[df["id"] != result["id"]]
+        # Add the updated row
+        df = pd.concat([df, pd.DataFrame([result])], ignore_index=True)
+
+        df.to_csv(report_all_runs_path, index=False)
+
+
+@app.command()
+def generate_task_table(
+    file_suffix: str = typer.Option(
+        "test",
+        help="File suffix for the reports",
+    ),
+    repeat: int = typer.Option(
+        2,
+        help="Number of times to repeat each url",
+    ),
+):
     df = pd.DataFrame(
-        data=results,
-        # columns=[
-        #     "url",
-        #     "summarizer_llm",
-        #     "websurfer_llm",
-        #     "websurfer_navigator_llm",
-        #     "time",
-        #     "success",
-        #     # "outer_retries",
-        #     "inner_retries",
-        #     "last_message",
-        # ],
+        columns=[
+            "id",
+            "url",
+            "time",
+            "success",
+            "summarizer_llm",
+            "websurfer_llm",
+            "websurfer_navigator_llm",
+            "inner_retries",
+            "last_message",
+            "status",
+        ]
     )
+
+    URLS = [
+        "https://www.ikea.com/gb/en/",
+        "https://www.disneystore.eu",
+        # "https://www.hamleys.com/",
+        # "https://www.konzum.hr",
+        # "https://faststream.airt.ai",
+    ]
+    URLS = URLS * repeat
+
+    df["url"] = URLS
+    df["id"] = range(len(URLS))
 
     file_name = _get_file_name(file_suffix)
     report_all_runs_path = REPORTS_FOLDER / file_name
-    report_all_runs_path_lock = REPORTS_FOLDER / f"{file_name}.lock"
-
-    with FileLock(str(report_all_runs_path_lock)):
-        # work with the file as it is now locked
-        header = not report_all_runs_path.exists()
-        df.to_csv(report_all_runs_path, mode="a", header=header, index=False)
-    report_all_runs_path_lock.unlink()
+    df.to_csv(report_all_runs_path, index=False)
 
 
 @app.command()
@@ -161,29 +200,85 @@ def run_tests(
         help="File suffix for the reports",
     ),
 ):
-    outer_retries: int = 1
+    # outer_retries: int = 1
 
-    URLS = [
-        "https://www.ikea.com/gb/en/",
-        "https://www.disneystore.eu",
-        "https://www.hamleys.com/",
-        "https://www.konzum.hr",
-        "https://faststream.airt.ai",
-    ]
+    while True:
+        url = ""
+        id = -1
+        with lock_file(file_suffix) as report_all_runs_path:
+            if not report_all_runs_path.exists():
+                print("Generating task table")
+                generate_task_table(file_suffix=file_suffix)
 
-    rng = np.random.default_rng()
-    urls = rng.permutation(URLS)
-    for url in urls:
-        result = run_test(
-            url=url,
-            outer_retries=outer_retries,
-            inner_retries=inner_retries,
-            summarizer_llm=summarizer_llm,
-            websurfer_llm=websurfer_llm,
-            websurfer_navigator_llm=websurfer_navigator_llm,
-        )
+            df = pd.read_csv(report_all_runs_path)
 
-        generate_reports(results=[result], file_suffix=file_suffix)
+            df_isna = df["status"].isna()
+            if not df_isna.any():
+                print("All tasks are done")
+                break
+
+            row = df[df_isna].iloc[0]
+            url = row["url"]
+            id = row["id"]
+
+            df["status"] = df["status"].astype(str)
+            df.loc[df["id"] == row["id"], "status"] = "PENDING"
+
+            # print(f"Updating the status of the task with url: {url}")
+
+            df.to_csv(report_all_runs_path, index=False)
+
+        print(f"Running test for {url}")
+
+        result = {
+            "url": url,
+            "time": 0,
+            "success": True,
+            "summarizer_llm": summarizer_llm,
+            "websurfer_llm": websurfer_llm,
+            "websurfer_navigator_llm": websurfer_navigator_llm,
+            "inner_retries": inner_retries,
+            "last_message": "vfvfvfvfv",
+            "status": "DONE",
+        }
+
+        # result = run_test(
+        #     url=url,
+        #     outer_retries=outer_retries,
+        #     inner_retries=inner_retries,
+        #     summarizer_llm=summarizer_llm,
+        #     websurfer_llm=websurfer_llm,
+        #     websurfer_navigator_llm=websurfer_navigator_llm,
+        # )
+        result["id"] = id
+
+        generate_reports(result=result, file_suffix=file_suffix)
+
+    #     generate_aggregated_report(file_suffix=file_suffix)
+
+    # URLS = [
+    #     "https://www.ikea.com/gb/en/",
+    #     "https://www.disneystore.eu",
+    #     # "https://www.hamleys.com/",
+    #     # "https://www.konzum.hr",
+    #     # "https://faststream.airt.ai",
+    # ]
+
+    # rng = np.random.default_rng()
+    # urls = rng.permutation(URLS)
+    # for url in urls:
+    #     result = run_test(
+    #         url=url,
+    #         outer_retries=outer_retries,
+    #         inner_retries=inner_retries,
+    #         summarizer_llm=summarizer_llm,
+    #         websurfer_llm=websurfer_llm,
+    #         websurfer_navigator_llm=websurfer_navigator_llm,
+    #     )
+
+    #     generate_reports(results=[result], file_suffix=file_suffix)
+
+    #     generate_aggregated_report(file_suffix=file_suffix)
 
 
 if __name__ == "__main__":
