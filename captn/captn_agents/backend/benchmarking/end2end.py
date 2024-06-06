bcf020039e3a86c7ecbb0dde5c32817f702c7adb
Robert Jambrecic <robert@airt.ai>
2024-06-06 16:42:01 +0200

    Fix tests

--- captn/captn_agents/backend/benchmarking/end2end.py
+++ captn/captn_agents/backend/benchmarking/end2end.py
@@ -12,7 +12,6 @@ from ..teams import (
 )
 from ..tools._brief_creation_team_tools import (
     _change_the_team_and_start_new_chat,
-    _get_info_from_the_web_page_original,
 )
 from .brief_creation_team import _client_system_messages, _get_task
 from .campaign_creation_team import (
@@ -53,7 +52,6 @@ def benchmark_end2end(
     url: str,
     llm: str = Models.gpt4o,
 ) -> Tuple[str, int]:
-    _get_info_from_the_web_page_original.cache_clear()  # type: ignore[attr-defined]
     config_list = get_config_list(llm)
 
     user_id = 123
