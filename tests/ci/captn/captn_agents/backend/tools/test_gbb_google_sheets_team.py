99075dd0acf74ef054aa5090480c220f2d6f9014
Robert Jambrecic <robert@airt.ai>
2024-07-15 10:38:00 +0200

    wip

--- tests/ci/captn/captn_agents/backend/tools/test_gbb_google_sheets_team.py
+++ tests/ci/captn/captn_agents/backend/tools/test_gbb_google_sheets_team.py
@@ -47,6 +47,40 @@ class TesteCreateGoogleAdsResources:
         ) as mock_get_sheet_data:
             yield mock_get_sheet_data
 
+    def test_create_google_ads_resources_with_missing_mandatory_columns(self) -> None:
+        keywords_values_with_missing_columns = {
+            "values": [
+                [
+                    "Campaign Budget",
+                    "Ad Group Name",
+                    "Match Type",
+                    "Keyword",
+                ],
+                [
+                    "10",
+                    "Pristina to Skoplje Transport | Exact",
+                    "Exact",
+                    "Svi autobusni polasci",
+                ],
+            ]
+        }
+        with unittest.mock.patch(
+            "captn.captn_agents.backend.tools._gbb_google_sheets_team_tools._get_sheet_data",
+            side_effect=[
+                ads_values,
+                keywords_values_with_missing_columns,
+            ],
+        ):
+            with pytest.raises(ValueError) as exc_info:
+                create_google_ads_resources(
+                    google_ads_resources=self.gads_resuces,
+                    context=self.context,
+                )
+            assert (
+                str(exc_info.value)
+                == "keywords_title is missing columns: ['Campaign Name']"
+            )
+
     def test_create_google_ads_resources(self, get_sheet_data_mock) -> None:
         with get_sheet_data_mock:
             response = create_google_ads_resources(
